50cf02240cb91dd618bcd9aabe7cd7ee73d55a5b
Himanshu12866 <manshu010m@gmail.com>
2024-08-11 21:50:25 +0530

    Modified

---
+++ learnreact/src/component/data3.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+
+export default function LoadData(){
+    const [product,setProduct] = useState({
+        
+        title:"",
+        price:0,
+        image : "",
+        offers:[],
+        rating: {
+            rate:0,
+            ratings:0,
+            reviews:0
+        }
+    })
+    function Fecth(){
+        fetch("data.json")
+        .then(response => response.json())
+        .then(
+            setProduct(product)
+        )
+    }
+    useEffect(() => {
+        Fecth();
+    })
+    return(
+        <div  className="row">
+        <div className="col-3">
+            <img src={product.image} alt="image" className="img-fluid" />
+        </div>
+        <div className="col-9">
+            <h1>{product.title}</h1>
+            <div>
+                <span className="bi bi-star-fill badge rounded bg-secondary">{product.rating.rate}</span>
+                <span>{product.rating.reviews}</span>
+                <span>{product.rating.ratings}</span>
+            </div>
+            <div>
+                {
+                    product.offers.map(
+                        (item,index) => {
+                            <p key={index}>{item}</p>
+                        }
+                    )
+                }
+            </div>
+        </div>
+
+        </div>
+    )
+}
