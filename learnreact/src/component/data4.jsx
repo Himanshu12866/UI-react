d375df17d5759d101c01f28a6884062ed1ae0e73
Himanshu12866 <manshu010m@gmail.com>
2024-08-12 16:28:25 +0530

    "Updated data.json and data4.jsx files: modified product object initialization, refactored fetch and useEffect logic, and adjusted JSX elements and styles."

--- learnreact/src/component/data4.jsx
+++ learnreact/src/component/data4.jsx
@@ -3,50 +3,47 @@ import { useEffect, useState } from "react";
 
 export default function LoadData1() {
     const [product, setProduct] = useState({
-        title:"",
-        price:0,
+        title: "",
+        price: 0,
+        image: "",
+        offers: [],
         rating: {
-            rate:0,
-            ratings:0,
-            reviews:0
-
-        },
-        image:"",
-        offers: []
-
-
-    });
-    function GetData() {
+            rate: 0,
+            ratings: 0,
+            reviews: 0
+        }
+    })
+    function FetchData() {
         fetch("data.json")
-            .then(response =>
-                response.json()
-            )
-            .then(product => {
-                setProduct(product);
-            })
+            .then(response => response.json())
+            .then(product => setProduct(product))
     }
+    useEffect(() => {
+        FetchData();
 
-    useEffect(()=>{
-        GetData();
-    },[])
-
+    }, [])
     return (
         <div className="row">
-            <div className="col-3">
-                <img alt="name" src={product.image} />
+            <div className="row-3">
+                <img src={product.image}></img>
             </div>
-            <div className="col-9" style={{textAlign:"left"}}>
+            <div className="col-9" style={{ textAlign: "left" }}>
                 <h1>{product.title}</h1>
-                
-                <span className="bi bi-star-fill badge rounded bg-dark" >{product.rating.rate}</span>
-                <span>{product.rating.ratings} Ratings</span><span>{product.rating.reviews} Reviews</span>
-            <div>
-                {
-                    product.offers.map((item,index) => 
-                        <p style={{textAlign:"left"}} className="text-dark" key={index}><span className="bi bi-tag-fill" style={{color:"blue"}}></span>{item}</p>
-                    )
-                }
-            </div>
+                <p>{
+                    product.price.toLocaleString('en-in', { style: "currency", currency: "INR" })
+                }</p>
+                <p>
+                    <span className="bi bi-star-fill">{product.rating.rate} Ratings </span>
+                    <span>{product.rating.ratings}</span><span>{product.rating.reviews}</span>
+                </p>
+                <div>
+                    {
+                        product.offers.map((item, index) =>
+                            <p style={{ textAlign: "left" }} key={index}><span className="bi bi-tag-fill"></span>{item}</p>
+
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
