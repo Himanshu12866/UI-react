034bf1fda2227b9ab98f5076b64486a7ef3c7a50
Himanshu12866 <manshu010m@gmail.com>
2024-08-12 16:08:14 +0530

    "Updated data.json and data4.jsx: changed reviews count in data.json, and refactored data4.jsx to initialize state with default values, updated JSX to display rating, reviews, and offers."

--- learnreact/src/component/data4.jsx
+++ learnreact/src/component/data4.jsx
@@ -1,33 +1,53 @@
 import { useEffect, useState } from "react";
 
 
-export default function LoadData1(){
-const [product ,setProduct] = useState({});
-function GetData(){
-    fetch("data.json")
-    .then(response => 
-    response.json()
+export default function LoadData1() {
+    const [product, setProduct] = useState({
+        title:"",
+        price:0,
+        rating: {
+            rate:0,
+            ratings:0,
+            reviews:0
+
+        },
+        image:"",
+        offers: []
+
+
+    });
+    function GetData() {
+        fetch("data.json")
+            .then(response =>
+                response.json()
+            )
+            .then(product => {
+                setProduct(product);
+            })
+    }
+
+    useEffect(()=>{
+        GetData();
+    },[])
+
+    return (
+        <div className="row">
+            <div className="col-3">
+                <img alt="name" src={product.image} />
+            </div>
+            <div className="col-9" style={{textAlign:"left"}}>
+                <h1>{product.title}</h1>
+                
+                <span className="bi bi-star-fill badge rounded bg-dark" >{product.rating.rate}</span>
+                <span>{product.rating.ratings} Ratings</span><span>{product.rating.reviews} Reviews</span>
+            <div>
+                {
+                    product.offers.map((item,index) => 
+                        <p style={{textAlign:"left"}} className="text-dark" key={index}><span className="bi bi-tag-fill" style={{color:"blue"}}></span>{item}</p>
+                    )
+                }
+            </div>
+            </div>
+        </div>
     )
-    .then(product => {
-        setProduct(product);
-    })
-}
-useEffect(() => {
-    GetData();
-})
-return(
-    <div className="row">
-        <div className="col-3">
-            <img alt="name" src={product.image}/>
-        </div>
-        <div className="col-9">
-            <h1>{product.title}</h1>
-            
-            
-               <p className="badge bg-primary"><span className="bi bi-star-fill">{product.rating.rate}</span></p>
-            
-      
-        </div>
-    </div>
-)
 }
