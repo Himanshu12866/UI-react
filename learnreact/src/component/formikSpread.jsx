c00d1074208bf763869796f995e2c5018084543a
Himanshu12866 <manshu010m@gmail.com>
2024-09-03 17:19:59 +0530

    "Formatted code in FormDemo3 component, added onBlur event handlers to input fields."

---
+++ learnreact/src/component/formikSpread.jsx
@@ -0,0 +1,20 @@
+ 
+
+
+ export default function FormDemo4(){
+    return (
+        <div className="divBox d-flex justify-content-center align-items-center w-75">
+            <div className="card w-50">
+                <div className="card-header text-center">
+                    <h1><span className="bi bi-person-fill"> </span>User Login</h1>
+                </div>
+                <div className="card-body">
+                    <form className="p-2">
+                        <label className="form-label">Userename :</label>
+                        <input className="form-control"></input>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+ }
