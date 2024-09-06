176167d8b43435504ef529e751ebc34f99b7116e
Himanshu12866 <manshu010m@gmail.com>
2024-09-06 19:01:38 +0530

    "Extracted employee data into a separate variable in useNav.jsx"

--- learnreact/src/component/custom/useNav.jsx
+++ learnreact/src/component/custom/useNav.jsx
@@ -4,10 +4,14 @@ import Table from "./table";
 
 
 export default function NavComponent() {
+    let employeeData =[
+            { "Name": "Himanshu Mishra", "Position": "Developer" , Sallery:"500000"}, 
+            { "Name": "Kritika Mishra", "Position": "Founder",Sallery: "100000" }
+        ]
     return (
         <div>
             <NavBar brand="Shopper." theme="bg-warning p-3" links={["Home", "About", "Docs", "Help", "Contact"]} />
             <NavBar brand="Shopper." theme="bg-info p-3 m-1" links={["Home", "About", "Docs", "Help", "Contact", "Learn", "Shop"]} />
-            <Table headItems={["Name", "Position", "Sallery"]} data={[{ "Name": "Himanshu Mishra", "Position": "Developer" , Sallery:"500000"}, { "Name": "Kritika Mishra", "Position": "Founder",Sallery: "100000" }]} />
+            <Table headItems={["Name", "Position", "Sallery"]} data={employeeData} />
         </div>)
 }
