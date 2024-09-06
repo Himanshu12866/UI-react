071a89ba194aaaffd4720a6ecdc6d1681ae74276
Himanshu12866 <manshu010m@gmail.com>
2024-09-06 16:27:27 +0530

    "Removed unused state variable, updated form validation schema, and refactored Captcha component code"

--- learnreact/src/component/captcha.jsx
+++ learnreact/src/component/captcha.jsx
@@ -8,7 +8,6 @@ export default function Captcha() {
     const [type, setType] = useState("password")
     const [show, setShow] = useState(false);
     const [eye, slashEye] = useState("bi bi-eye-slash");
-    // const [err , errorCaptch] = useState("")
 
     const formik = useFormik({
         initialValues: {
@@ -16,50 +15,37 @@ export default function Captcha() {
             psw: "",
             Mobile: "",
             city: "",
-            codeCaptcha:""
+            codeCaptcha: ""
         },
         validationSchema: yup.object({
             name: yup.string().required("Enter Username"),
-
             Mobile: yup.string().required("Please Enter Valid Number").matches(/\w{10}/, "Please Enter Valid number"),
             city: yup.string().required("Please Select City"),
-            codeCaptcha:yup.string().required("Please Enter the captcha code").max(6).matches(otp , "Please enter the correct code")
-        })
+            psw: yup.string().required("Please Enter Password").matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/,"Atlease one Uppercase , atleast one lowercase , atleaset one muber , atleast one special character"),
+            codeCaptcha: yup.string().required("Please Enter the captcha code").max(6).matches(otp, "Please enter the correct code")
+        }),
+        onSubmit : (values) => {
+            console.log(values)
+        }
     })
     function ChangeCode() {
-
         let code = useCapcha()
         seOtp(code);
     }
-
     function ChangeType() {
-
         if (show) {
             setType("text")
             setShow(false)
             slashEye("bi bi-eye text-light")
-
-
         } else {
             setType("password")
             setShow(true);
             slashEye("bi bi-eye-slash")
-
         }
     }
-// function MatchCaptcha(e){
-//     if(e.target.value !== otp){
-// errorCaptch("Please Enter A Correct Code")
-//     }
-//     else{
-//         errorCaptch("")
-//     }
-// }
-
     return (
         <div className="d-flex justify-content-center">
             <div className="card mt-2 w-75">
-
                 <div className="card-header bg-info text-light text-center">
                     <h1>Registration Form</h1>
                 </div>
@@ -69,13 +55,12 @@ export default function Captcha() {
                         <input type="text" onChange={formik.handleChange} className="form-control my-1" name="name" />
                         <p className="text-danger">{formik.errors.name}</p>
                         <label className="form-label">Password:</label>
-
                         <div className="input-group">
-                            <input className="form-control my-1" type={type} name="psw"></input>
+                            <input className="form-control my-1" type={type} onChange={formik.handleChange} name="psw"></input>
                             <span className={`btn btn-secondary input-group-text my-1  ${eye}`} onClick={ChangeType}></span>
+                    
                         </div>
-
-                        <p className="text-danger">{ }</p>
+                        <p className="text-danger">{formik.errors.psw }</p>
                         <label className="form-label">Mobile No. :</label>
                         <input className="form-control my-1" type="text" name="Mobile" onChange={formik.handleChange}></input>
                         <p className="text-danger">{formik.errors.Mobile}</p>
@@ -87,11 +72,10 @@ export default function Captcha() {
                             <option value="hyd">Hyderabad</option>
                         </select>
                         <p className="text-danger">{formik.errors.city}</p>
-                       
                         <div className="d-flex justify-content-between my-2">
                             <label className="form-label">Please Enter The Captcha :</label> <button onClick={ChangeCode} className="btn btn-secondary bi bi-arrow-counterclockwise"></button>
                         </div>
-                        <input type="text" className="form-control"  name="codeCaptcha" required onChange={formik.handleChange} ></input>
+                        <input type="text" className="form-control" name="codeCaptcha" onChange={formik.handleChange} ></input>
                         <p className="text-danger">{formik.errors.codeCaptcha}</p>
                         <button className="btn btn-dark text-light w-100 my-3" style={{ letterSpacing: "1px" }}>{otp}</button>
                         <div>
