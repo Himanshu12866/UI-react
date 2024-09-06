808756e443d2a9c8751f9be9e96930946881fd27
Himanshu12866 <manshu010m@gmail.com>
2024-09-06 17:01:42 +0530

    "Updated GeneratePsw function to generate 12-character password, renamed it to useGeneratePsw, and integrated it into Captcha component with Generate button and password display."

--- learnreact/src/component/Hooks/generatepsw.js
+++ learnreact/src/component/Hooks/generatepsw.js
@@ -1,8 +1,8 @@
-export default function GeneratePsw(){
+export default function useGeneratePsw(){
     let pws = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123654789@$%&*";
     let generatedpsw = "";
-    for (let i = 0; i < 8; i++) {
-        let random = Math.floor(Math.random() * 67);
+    for (let i = 0; i < 12; i++) {
+        let random = Math.floor(Math.random() * 66);
         generatedpsw += pws[random];
     }
     return generatedpsw;
--- learnreact/src/component/captcha.jsx
+++ learnreact/src/component/captcha.jsx
@@ -2,12 +2,14 @@ import useCapcha from "./Hooks/captch"
 import { useFormik } from "formik"
 import { useState } from "react";
 import *as yup from "yup";
+import useGeneratePsw from "./Hooks/generatepsw";
 
 export default function Captcha() {
     const [otp, seOtp] = useState(" OTP");
     const [type, setType] = useState("password")
     const [show, setShow] = useState(false);
     const [eye, slashEye] = useState("bi bi-eye-slash");
+    const [psword, setPsword] = useState("....")
 
     const formik = useFormik({
         initialValues: {
@@ -21,10 +23,10 @@ export default function Captcha() {
             name: yup.string().required("Enter Username"),
             Mobile: yup.string().required("Please Enter Valid Number").matches(/\w{10}/, "Please Enter Valid number"),
             city: yup.string().required("Please Select City"),
-            psw: yup.string().required("Please Enter Password").matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/,"Atlease one Uppercase , atleast one lowercase , atleaset one muber , atleast one special character"),
+            psw: yup.string().required("Please Enter Password").matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/, "Atlease one Uppercase , atleast one lowercase , atleaset one muber , atleast one special character"),
             codeCaptcha: yup.string().required("Please Enter the captcha code").max(6).matches(otp, "Please enter the correct code")
         }),
-        onSubmit : (values) => {
+        onSubmit: (values) => {
             console.log(values)
         }
     })
@@ -43,6 +45,16 @@ export default function Captcha() {
             slashEye("bi bi-eye-slash")
         }
     }
+    function Generate() {
+        let psw = useGeneratePsw();
+        setPsword(psw);
+
+    }
+    function Restrict(){
+    document.oncontextmenu = () => {
+        return false;
+    }
+    }
     return (
         <div className="d-flex justify-content-center">
             <div className="card mt-2 w-75">
@@ -59,11 +71,11 @@ export default function Captcha() {
                             <input className="form-control my-1" type={type} onChange={formik.handleChange} name="psw"></input>
                             <span className={`btn btn-secondary input-group-text my-1  ${eye}`} onClick={ChangeType}></span>
                         </div>
-                        <div className="d-flex justify-content-between my-2 mx-2">
-                            <button className="btn btn-dark w-50">psw</button>
-                            <button className="btn btn-success bi bi-pencil-square w-25 p-1"> Generate</button>
+                        <div className="d-flex justify-content-between my-2  mx-2">
+                            <p style={{ backgroundColor: "green", color: "black", padding: "14px" }} className="w-25 text-center text-light">{psword}</p>
+                            <p style={{backgroundColor:"red", textAlign:"center" , padding:"14px" , color:"white" , cursor:"pointer"}} className="bi bi-pencil-square w-25" onCopy={Restrict} onClick={Generate}> &nbsp; Generate Password</p>
                         </div>
-                        <p className="text-danger">{formik.errors.psw }</p>
+                        <p className="text-danger">{formik.errors.psw}</p>
                         <label className="form-label">Mobile No. :</label>
                         <input className="form-control my-1" type="text" name="Mobile" onChange={formik.handleChange}></input>
                         <p className="text-danger">{formik.errors.Mobile}</p>
