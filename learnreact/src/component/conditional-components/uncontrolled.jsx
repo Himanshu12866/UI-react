67377488a5a23b0045cb35533fd71aa352304ffc
Himanshu12866 <manshu010m@gmail.com>
2024-09-13 16:41:04 +0530

    "Updated Uncontrolled component: refactored navbar, added list items, and reformatted code"

--- learnreact/src/component/conditional-components/uncontrolled.jsx
+++ learnreact/src/component/conditional-components/uncontrolled.jsx
@@ -3,32 +3,39 @@ import { useState } from "react";
 
 export default function Uncontrolled() {
     const [sigIn, seSignOut] = useState(false);
-    function handleSignIn(){
+    function handleSignIn() {
         seSignOut(true);
     }
-    function handleSignOut(){
+    function handleSignOut() {
         seSignOut(false)
     }
 
     return (
-        <div>
+        <div className="navlist">
             <nav className="navbar">
                 <a className="navbar-brand">Shopper.</a>
-                
-                    {
-                        (sigIn === true) ?
 
-                            <ol className="navbar-nav">
-                              <span>Home</span>
-                              <span>Home</span>
-                              <span>Home</span>
-                              <span>Home</span>
-                              <span>Home</span>
-                            </ol>
-                            :
-                            <div></div>
-                    }
-               
+                {
+                    (sigIn === true) ?
+
+                        <ol className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">Help</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">Contact</a>
+                            </li>
+                        </ol>
+                        :
+                        <div></div>
+                }
+
                 <ol>
                     {
                         (sigIn === true) ? <button className="btn btn-dark" onClick={handleSignOut}>Sign Out</button> : <button className="btn btn-success" onClick={handleSignIn}>Sign In</button>
