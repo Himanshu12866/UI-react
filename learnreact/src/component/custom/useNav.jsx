bdd4213b60688f502efbc1ea9c534954bccf5b17
Himanshu12866 <manshu010m@gmail.com>
2024-09-13 15:15:44 +0530

    "Updated NavComponent: added new employee data, modified NavBar theme, and added 'Role' column to Table"

--- learnreact/src/component/custom/useNav.jsx
+++ learnreact/src/component/custom/useNav.jsx
@@ -6,12 +6,13 @@ import Table from "./table";
 export default function NavComponent() {
     let employeeData =[
             { "Name": "Himanshu Mishra", "Position": "Developer" , Sallery:"500000"}, 
-            { "Name": "Kritika Mishra", "Position": "Founder",Sallery: "100000" }
+            { "Name": "Kritika Mishra", "Position": "Founder",Sallery: "100000" },
+            { "Name": "Rahul Mishra", "Position": "Developer",Sallery:"150k" , Role:"Team Leader"}
         ]
     return (
         <div>
-            <NavBar brand="Shopper." theme="bg-warning p-3" links={["Home", "About", "Docs", "Help", "Contact"]} />
+            <NavBar brand="Shopper." theme="bg-warning p-3 text-light" links={["Home", "About", "Docs", "Help", "Contact"]} />
             <NavBar brand="Shopper." theme="bg-info p-3 m-1" links={["Home", "About", "Docs", "Help", "Contact", "Learn", "Shop"]} />
-            <Table headItems={["Name", "Position", "Sallery"]} data={employeeData} />
+            <Table headItems={["Name", "Position", "Sallery" , "Role"]} data={employeeData} />
         </div>)
 }
