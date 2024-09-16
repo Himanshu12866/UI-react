8bb77d0da17e8f9b0a33bb36390f059ee83026b7
Himanshu12866 <manshu010m@gmail.com>
2024-09-16 15:05:39 +0530

    "Updated DraggableBox component: renamed CSS class, added state variables and event handler for drag functionality, and modified JSX elements"

--- learnreact/src/component/draggable.jsx
+++ learnreact/src/component/draggable.jsx
@@ -1,11 +1,21 @@
 
+import { useState } from "react"
 import "../component/draggable.css"
-export default function DraggableBox(){
+export default function DraggableBox() {
+    const [left, setLeft] = useState("")
+    const [top, setTop] = useState("")
+    function handleDrag(e) {
+        let x = e.clientX;
+        let y = e.clientY;
+        console.log(x)
+        console.log(y)
+        setLeft(x)
+        setTop(y)
+    }
 
-
-    return(
-        <div className="drag-box">
-        <div className="dragbox"></div>
+    return (
+        <div className="drag-box" onMouseMove={handleDrag} >
+            <div className="dragbox" style={{ left: { left }, top: { top } }}></div>
 
         </div>
     )
